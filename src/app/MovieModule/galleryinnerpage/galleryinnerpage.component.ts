c0511897df4bb63030a46397cec0f402d9ee1bc7
GUI TEAM <GUI TEAM>
2017-03-03 03:01:41 +0530

    kdjfkj

--- src/app/MovieModule/galleryinnerpage/galleryinnerpage.component.ts
+++ src/app/MovieModule/galleryinnerpage/galleryinnerpage.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SharedService} from '../../../Provider/sharedservice.service'
+import * as Firebase from 'firebase';
+declare var FB;
+declare var window:any;
 
 @Component({
   selector: 'app-galleryinnerpage',
@@ -7,16 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryinnerpageComponent implements OnInit {
 
-  constructor() { }
+   public gallery
+  public videourl;
+  public starsid;
+  public share:boolean=false;
+  public rate:boolean=false;
+  public userid;
+  public galleryimagesdvey;
+  constructor(private router: ActivatedRoute, private sanitizer: DomSanitizer,private sharedService:SharedService,private routernav:Router) { 
 
+
+  }
   ngOnInit() {
+    this.sharedService.sharedvalue.category="Hollywood";
+    
+     if(firebase.auth().currentUser!=null)
+     {
+       this.userid=firebase.auth().currentUser.uid;
+       
+     }
+    this.router.params.subscribe((params) => {
+     
+      const id = params['id'];
+      
+      
+      var self=this;
+      var adbcd_movie=firebase.database().ref('posts/Gallery/dev/').child(id);
+    adbcd_movie.on("value", function(snapshot) {
+    self.gallery=snapshot.val();
+    self.gallery.id=snapshot.key;
+       
+
+
+       
+     }, function (errorObject) {
+   console.log("The read failed: " + errorObject.code);
+   });})
+
+
+
      this.kk()
   }
-ngAfterViewInit()
-{
-  // this.kk();
-  // console.log('kkk')
-}
+
+
+
   kk()
   {
       var api=(<any>jQuery('.megafolio-container')).megafoliopro(
@@ -48,5 +88,49 @@ ngAfterViewInit()
 		var duration = 500;	
 	
   }
+
+  addFavourite(gallery,id)
+  {
+   
+    if(firebase.auth().currentUser!=null)
+   {
+    
+     var newPostKey = firebase.database().ref().child('posts').push().key;
+     var updates = {};
+     var recentPostsRef = firebase.database().ref('posts/News/dev/'+id);   
+     var uid = firebase.auth().currentUser.uid;
+     
+     firebase.database().ref().update(updates);     
+     updates['/Favourites/gallery/' +uid+'/'+newPostKey]=gallery; 
+     firebase.database().ref().update(updates);   
+     this.sharedService.favourite(uid,recentPostsRef); 
+   }
+   else
+   {
+     this.routernav.navigate(['Signin']);
+   }
+  }
+
+  like(id)
+{ 
+    
+   if(firebase.auth().currentUser!=null)
+   {
+    var uid = firebase.auth().currentUser.uid;
+    var recentPostsRef = firebase.database().ref('posts/News/dev/'+id);
+    this.sharedService.LikethePost(recentPostsRef,uid);  
+   }
+   else
+   {
+     this.routernav.navigate(['Signin']);
+   }
+}
+
+  
+  ngOnDestroy()
+{
+  this.gallery=[];
+}
+
 
 }
